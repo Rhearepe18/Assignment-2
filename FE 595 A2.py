8c368739d5df2b7bc0ee2f2165220353715e177a
Rhearepe18 <54646480+Rhearepe18@users.noreply.github.com>
2019-11-19 22:30:55 -0500

    Update FE 595 A2.py

--- FE 595 A2.py
+++ FE 595 A2.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[173]:
-
-
 from flask import Flask
 import requests
 from bs4 import BeautifulSoup   #The Beautiful Soup package is used to extract data from html files. 
@@ -11,67 +6,35 @@ from urllib.request import urlopen  #The urllib.request module is used to open U
 import re 
 from requests import get
 
-
-# In[174]:
-
-
 #After importing necessary modules, 
 #specify the URL containing the dataset and pass it to urlopen() to get the html of the page.
 
 url = "https://theyfightcrime.org/"
 html = urlopen(url)
-        
-    
+   
 
 text_html = requests.get('https://theyfightcrime.org/')   #Make request
 
-
-# In[175]:
-
-
 #Make a request to “theyfightcrime.org”   
-
 
 print(text_html.text)   #Print the HTML code
 
-
-# In[176]:
-
-
-
-
 soup = BeautifulSoup(text_html.text, 'lxml')
-
-
-# In[177]:
-
 
 #Remove all style and font elements
 
 for script in soup(["script", "style"]):
     script.extract() 
 
-
-# In[178]:
-
-
 text = soup.get_text()
 print(text)  #Prints only text on website
 type(text)
-
-
-# In[179]:
 
 
 #We want to generate two files.
 #Save the male characters in one text file and save the female characters in another text file.
 
 #Repeat 50 times (once done above)
-
-
-# In[193]:
-
-
 
 def ws(url):       #Define a function 
     male = open("Male.txt","w+")      #The open() function opens a file, and returns it as a file object                     
@@ -89,22 +52,6 @@ def ws(url):       #Define a function
 
     male.close()  
     female.close()                                        
-
-
-
-    
-    
-    
-
-
-# In[194]:
-
-
-##I have used various sources for the above code like datacamp.com(first part). 
-#The last part to write files and extracting the words was looked up on google and github (never used beautifulSoup before)
-
-
-# In[195]:
 
 
 ws("https://theyfightcrime.org/")
